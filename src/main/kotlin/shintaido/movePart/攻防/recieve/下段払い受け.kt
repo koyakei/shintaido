5b5006e29bbaa906e5bb9b720134a827658e8470
koyanagi <koyakeiaaaaa@gmail.com>
2021-09-22 21:55:08 +0900

    刃の向きの定義をリネーム

--- src/main/kotlin/shintaido/movePart/攻防/recieve/下段払い受け.kt
+++ src/main/kotlin/shintaido/movePart/攻防/recieve/下段払い受け.kt
@@ -2,13 +2,13 @@ package shintaido.movePart.攻防.recieve
 
 import shintaido.movePart.攻防.刃の向き.刃の向き
 import shintaido.movePart.攻防.動かし方.動かし方
-import shintaido.movePart.攻防.動きの向き.技の動きの向き
+import shintaido.movePart.攻防.動きの向き.刃の向きで相対的に定義する切るの向き
 import shintaido.movePart.攻防.防御技
 import shintaido.movePart.構え.上体の構えの高さ
 
 class 下段払い受け : 防御技 {
     val 刃の向き : 刃の向き = 刃の向き(shintaido.movePart.攻防.刃の向き.刃の向き.順刃)
-    val 技の動きの向き: 技の動きの向き = 技の動きの向き()
+    val 刃の向きで相対的に定義する切るの向き: 刃の向きで相対的に定義する切るの向き = 刃の向きで相対的に定義する切るの向き()
     val 上体の構えの高さ: 上体の構えの高さ = shintaido.movePart.構え.上体の構えの高さ.下段
     val 動かし方: 動かし方 = shintaido.movePart.攻防.動かし方.動かし方.払い
 }
--- src/main/kotlin/shintaido/movePart/攻防/動きの向き/刃の向きで相対的に定義する切るの向き.kt
+++ src/main/kotlin/shintaido/movePart/攻防/動きの向き/刃の向きで相対的に定義する切るの向き.kt
@@ -3,15 +3,14 @@ package shintaido.movePart.攻防.動きの向き
 
 /**
  * 刃の方向に対して何度で動かすのか？
- * 順て
  * 棟の方向に切るのか？
  */
-class 技の動きの向き(val direction: Direction = 順斬り) {
+class 刃の向きで相対的に定義する切るの向き(val direction: Direction = 順斬り) {
     @JvmInline
     value class Direction(val int: Int)
 
     companion object {
         val 順斬り = Direction(0)
-        val 逆手斬り = Direction(180)
+        val 棟受け = Direction(180)
     }
 }
