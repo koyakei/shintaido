69decce618c96ccc9d6a5af2f49a31ab4a6d9a71
koyanagi <koyakeiaaaaa@gmail.com>
2022-09-21 00:27:53 +0900

    fix comment

--- src/main/kotlin/ridingSports/balance/RightPositionOfCenterOfMass.kt
+++ src/main/kotlin/ridingSports/balance/RightPositionOfCenterOfMass.kt
@@ -5,11 +5,8 @@ import library.Vector
 interface RightPositionOfCenterOfMass {
 
     /**
-     * 重力方向と慣性系の合成で下方向を決定
-     *
+     * 重力方向と乗り手にかかる加速度の合成で下方向を決定
      * 支持基底点と鉛直上に重心を設定可能な範囲を線で表現
-     *
-     *
      */
     fun handle(gravityVector: Vector, ridersAccelerationVector: Vector): Vector{
         return gravityVector + ridersAccelerationVector
